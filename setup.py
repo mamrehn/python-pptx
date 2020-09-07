226f2ffbf70565b2b1f77610a590e3b676eec22c
mamrehn <mamrehn@users.noreply.github.com>
2020-09-07 23:05:26 +0200

    Add Python v3.7 and v3.8.

--- setup.py
+++ setup.py
@@ -58,6 +58,8 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Topic :: Office/Business :: Office Suites",
     "Topic :: Software Development :: Libraries",
 ]
